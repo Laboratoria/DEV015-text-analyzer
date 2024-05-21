c1e59c8dfd003a004db20c2b1afea8e0eb22dbd8
Aben <126703861+abengl@users.noreply.github.com>
2024-05-21 11:16:05 -0500

    Cambios en JS y CSS

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,6 +1,20 @@
+/*
+La aplicación usa el selector del DOM querySelector.
+La aplicación usa el selector del DOM getElementById.
+La aplicación registra un Event Listener para escuchar el evento input del <textarea> para actualizar las métricas cuando se haga escriba en el cuadro de texto.
+ La aplicación registra un Event Listener para escuchar el evento click del <button> que limpia el contenido de la caja de texto.
+  La aplicación actualiza el atributo textContent o innerHTML de los <li> que mostrar las métricas del texto.
+*/
+
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const wordsArray = text.split(" ");
+    console.log(wordsArray);
+    //property that returns the size of an array as a number(integer).
+    const wordsCount = wordsArray.length;
+    console.log(wordsCount);
+    return wordsCount;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const ulElement = document.querySelector("ul");
+  
+const liElements = ulElement.querySelectorAll("li")
+  
+let wordCounterLi = liElements[0];
+let charCounterLi = liElements[1];
+let numCounterLi = liElements[2];
+let charSpacesCounterLi = liElements[3];
+let averageLengthCounterLi = liElements[4];
+let sumCounterLi = liElements[5];
+
+//La aplicación registra un Event Listener para escuchar el evento input del <textarea> para actualizar las métricas cuando se haga escriba en el cuadro de texto.
+const textArea =document.querySelector("[name='user-input']");
+
+textArea.addEventListener("input", retrieveText);
+
+//La aplicación actualiza el atributo textContent o innerHTML de los <li> que mostrar las métricas del texto.
+function retrieveText(){
+  let textInput = textArea.value;
+  console.log("text input: " + textInput);
+  let wordCounter = analyzer.getWordCount(textInput);
+  console.log("counter of words: " + wordCounter);
+
+  wordCounterLi.textContent = "Palabras " + wordCounter;
+}
+
+/*
+  querySelectorCalls,
+  getElementByIdCalls,
+  addEventListenerCalls,
+  importStatements,
+  textContents,
+  innerHTMLs
+  */
+
+
+
+
+
+//La aplicación registra un Event Listener para escuchar el evento click del <button> que limpia el contenido de la caja de texto.
+const button = document.getElementById("reset-button");
+
+button.addEventListener("click", clearTextArea);
+
+function clearTextArea(){
+    textArea.value = "";
+
+}
+
