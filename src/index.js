20542bdce51c662dde17c36af7af8960cada72c4
fe-ercg <mabfher@gmail.com>
2024-05-27 17:33:00 -0500

    fact: add function to clear button

--- src/index.js
+++ src/index.js
@@ -5,30 +5,36 @@ const textarea = document.getElementsByName("user-input")[0];
 const list = document.querySelector("ul");
 const listItems = list.querySelectorAll("li");
 
+const buttonClean = document.getElementById('reset-button');
+
 textarea.addEventListener("input", function () {
   const input = textarea.value;
-
+  
   // WORD COUNT
   const wordCount = analyzer.getWordCount(input);
   listItems[0].innerText = `Palabras: ${wordCount}`;
-
+  
   // CHARACTER COUNT
   const characterCount = analyzer.getCharacterCount(input);
   listItems[1].innerText = `Caracteres: ${characterCount}`;
-
+  
   // CHARACTER COUNT EXCLUDING
   const characterCountExcluding = analyzer.getCharacterCountExcludingSpaces(input);
   listItems[2].innerText = `Caracteres sin espacios: ${characterCountExcluding}`;
-
+  
   // AVERAGE WORDS
   const averageWords = analyzer.getAverageWordLength(input);
   listItems[3].innerText = `Promedio longitud: ${averageWords}`;
-
+  
   // NUMBER COUNT
   const numberCount = analyzer.getNumberCount(input);
   listItems[4].innerText = `Numeros: ${numberCount}`;
-
+  
   // NUMBER SUM
   const numberSum = analyzer.getNumberSum(input);
   listItems[5].innerText = `Suma de los numeros: ${numberSum}`;
+});
+
+buttonClean.addEventListener('click', function(){
+  textarea.value='';
 });
