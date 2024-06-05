a32f42c62d99adf10d9a68b74b4d6e26c8f9526f
Gema Riffo <gema.r.y382@gmai.com>
2024-06-05 18:57:04 -0400

    actualizaciones

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,33 @@
 const analyzer = {  
+
   getWordCount: (text) => {
+      let palabras = 0
+      for (let i = 0; i <text.length; i++) {  //ver arrays el for e if no es
+        if(i === 0  (text[i - 1]) === " ") {
+          palabras++; 
+          console.log(palabras)
+        }
+      }
+      return palabras;
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+ // getCharacterCount: (text) => {
+   // return text.length
+    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro text de tipo string.
   },
-  getCharacterCountExcludingSpaces: (text) => {
+  ///getCharacterCountExcludingSpaces; (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getAverageWordLength: (text) => {    
+ // },
+  //getAverageWordLength; (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getNumberCount: (text) => {
+  //},
+  //getNumberCount; (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getNumberSum: (text) => {
+ // },
+  //getNumberSum; (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
+  //},
 };
+
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const textArea=document.querySelector('textarea[name=".user-input"]');
+
+const palabras = document.querySelector("item[data-testid='word-count']");
+const caracteres = document.querySelector("item[data-testid='character-count']");
+const caracteressinespacio = document.querySelector("item[data-testid='character-no-space-count']");
+const numeros = document.querySelector("item[data-testid='numbers']");
+const sumanumeros = document.querySelector("item[data-testid='number-sum']");
+const longitudpalabra = document.querySelector("item[data-testid='word-length-average']")
+
+
+const resetButton = document.getElementById("reset-button");
+let userInput = document.getElementsByName("user-input")[0];
+//console.log(document.getElementsByName("user-input"))
+function Limpiar() {
+    userInput.value = "";  // Limpiar el textarea
+}
+
+resetButton.addEventListener('click', Limpiar);
+
+function actualizar() {
+let texto = textArea.value;
+
+
+let totalpalabras = analyzer.getWordCount(texto);
+palabras.textContent = "Palabras: " + totalpalabras;
+
+let totalcaracteres = analyzer.getCharacterCount(texto);
+caracteres.textContent = "Caracteres: " + totalcaracteres;
+
+let totalcaracteressinespacio = analyzer.getCharacterCountExcludingSpaces(texto);
+caracteressinespacio.textContent = "Caracteres sin Espacio: " + totalcaracteressinespacio;
+
+let totalnumeros = analyzer.getNumbersCount(texto);
+numeros.textContent = "Números: " + totalnumeros;
+
+let totalsumanumeros = analyzer.getNumberSum(texto);
+sumanumeros.textContent = "Suma Números: " + totalsumanumeros;
+
+let totallongitudpalabra = analyzer.getAverageWordLength(texto);
+longitudpalabra.textContent = "Promedio Longitúd Palabra" + totallongitudpalabra; 
+}
+
+
